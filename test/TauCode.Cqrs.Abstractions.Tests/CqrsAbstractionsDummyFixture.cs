65b10030fca8155bfd606b9f92e102a1bd2f8176
Andrey Kovalenko <ak@taucode.com>
2020-10-08 16:40:16 +0300

    Dummy fixture.

---
+++ test/TauCode.Cqrs.Abstractions.Tests/CqrsAbstractionsDummyFixture.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.Cqrs.Abstractions.Tests
+{
+    [TestFixture]
+    public class CqrsAbstractionsDummyFixture
+    {
+        [Test]
+        public void CqrsAbstractionsDummyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Pass("Added for Azure DevOps compliance.");
+        }
+    }
+}
